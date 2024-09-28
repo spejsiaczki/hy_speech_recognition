f993174a29bb1aef1be498aec4b40526e942a9d9
Szymon Bednorz <dsonyyy@gmail.com>
2024-09-28 19:55:08 +0200

    add language detection but issues for short words

--- speech.py
+++ speech.py
@@ -4,6 +4,7 @@ import speech_recognition
 from readability import Readability
 from typing import List, Tuple
 import nltk
+from langdetect import detect_langs, LangDetectException
 
 
 logger = logging.getLogger(__name__)
@@ -98,8 +99,14 @@ class SpeechProcessingPipeline:
         return timestamps
 
     def get_text(self) -> str:
-        return "".join(
-            segment["text"] for segment in self.results["segments"])
+        return "".join(self.get_words())
+
+    def get_words(self) -> List[str]:
+        words = []
+        for segment in self.results["segments"]:
+            for word in segment["words"]:
+                words.append(word["word"])
+        return words
 
     def get_pause_timestamps(self) -> List[Tuple[float, float]]:
         pauses = []
@@ -123,39 +130,39 @@ class GunningFog():
         r = Readability(t)
         self._gf = r.gunning_fog()
 
-    @property
+    @ property
     def score(self) -> float:
         if not self._gf.score:
             self.run()
         return self._gf.score
 
-    @property
+    @ property
     def grade_level(self) -> str:
         if not self._gf.grade_level:
             self.run()
         return self._gf.grade_level
 
-    @property
+    @ property
     def grade_level_pl(self) -> str:
         score = self.score
         if 0 <= score < 7:
-            return "język bardzo prosty," \
-                "zrozumiały już dla uczniów szkoły podstawowej"
+            return "język bardzo prosty,"
+            "zrozumiały już dla uczniów szkoły podstawowej"
         elif 7 <= score < 10:
-            return "język prosty," \
-                " zrozumiały już dla uczniów gimnazjum"
+            return "język prosty,"
+            " zrozumiały już dla uczniów gimnazjum"
         elif 10 <= score < 13:
-            return "język dość prosty," \
-                "zrozumiały już dla uczniów liceum"
+            return "język dość prosty,"
+            "zrozumiały już dla uczniów liceum"
         elif 13 <= score < 16:
-            return "język dość trudny, " \
-                "zrozumiały dla studentów studiów licencjackich"
+            return "język dość trudny, "
+            "zrozumiały dla studentów studiów licencjackich"
         elif 16 <= score < 18:
-            return "język trudny, " \
-                "zrozumiały dla studentów studiów magisterskich"
+            return "język trudny, "
+            "zrozumiały dla studentów studiów magisterskich"
         elif 18 <= score:
-            return "język bardzo trudny, " \
-                "zrozumiały dla magistrów i osób z wyższym wykształceniem"
+            return "język bardzo trudny, "
+            "zrozumiały dla magistrów i osób z wyższym wykształceniem"
 
     def _multiply_sample(self, sample: str):
         """
@@ -167,10 +174,35 @@ class GunningFog():
         return sample * multiply
 
 
+class LanguageDetection:
+    def __init__(self, text: str):
+        self.text = text
+        self.possible_languages = self._detect_language()
+
+    def _detect_language(self):
+        """
+        Returns a list of detected languages with confidence scores
+        """
+        if any(ch in self.text for ch in "ęóąśłżźćń"):
+            return [("pl", 1.0)]
+
+        if len(self.text) < 5:
+            return []
+
+        try:
+            return [(lang.lang, lang.prob) for lang in detect_langs(self.text)]
+        except LangDetectException:
+            return []
+
+    @property
+    def languages(self):
+        return self.possible_languages
+
+
 if __name__ == "__main__":
     logger.info("Starting video to audio pipeline")
 
-    input = "./data/HY_2024_film_02.mp4"
+    input = "./data/HY_2024_film_17.mp4"
     temp = "data_processed/output.wav"
 
     VideoToAudioPipeline(input, temp).run()
@@ -180,6 +212,10 @@ if __name__ == "__main__":
     print(speach_processing.get_word_timestamps())
     print(speach_processing.get_text())
     print(speach_processing.get_pause_timestamps())
+
+    for word in speach_processing.get_words():
+        lang_detect = LanguageDetection(word)
+        print(word, lang_detect.languages)
 
     TEXT = """Mam ciągle w uszach głos, twój ciepły głos i oczy ciągle ciebie pełne mam, a już pod stopą moją dudni, dudni most; przez wiatr, przez mróz, przez słońce i przez zieleń maszeruję."""
 
